e75a500fd9f099808b849b32f8d9cc8f45915024
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-07-21 16:14:18 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,6 +6,6 @@ end
 def introduction_with_language(name , lang)
   puts "Hi, my name is #{name} and I am learning to program in #{lang}."
 end 
-def introduction_with_language_optional(name, lang)
+def introduction_with_language_optional(name, lang = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{lang}."
 end
